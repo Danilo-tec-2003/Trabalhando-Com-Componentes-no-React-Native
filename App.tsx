51e28f42b46c11f63e2fb41af87011e13b8b4153
Danilo Mendes <danilodev.br@gmail.com>
2024-08-13 19:21:40 -0300

    primeiro commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Image,TextInput, StyleSheet, Text, View, Button, ScrollView, Switch} 
+from 'react-native';
+import React,{useState} from 'react'
+import imgSuper from './assets/super.png'
 
 export default function App() {
+  const [usuario, setUsuario] = useState('A')
+  const [ligado, setLigado] = useState(true) 
+
+  function handleSwitch(){
+    setLigado(!ligado)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <View style={[styles.container, {backgroundColor:'red'}]}>
+        
+        <Switch 
+          value={ligado}
+          onValueChange={handleSwitch}
+          >
+        </Switch>
+
+        <Image
+          source={imgSuper}
+          style={{display: ligado ? "flex" : "none"}}
+        />
+        <TextInput
+          style={styles.input}
+          onChange={(text)=>setUsuario(text.nativeEvent.text)}
+          keyboardType="default"
+          placeholder='digite seu numero'
+          value={usuario}
+        />
+        <Button
+          title='click aqui'
+          onPress={()=>{Alert.alert('valor atual', usuario)}}
+        />
+
+
+        <View
+          onTouchStart={(event)=>{
+            Alert.alert('TOQUE', 'Clique iniciado')
+          }}
+          onTouchEnd={(event)=>{
+            Alert.alert('TOQUE', 'Toque finalizado')
+          }}
+        >
+        <Text style={[styles.texto, styles.border]}>
+            oi
+        </Text>
+        </View>
+        
+          <Text
+            selectable={false}
+            onPress={()=>{console.log("pressionado")}}
+            onLongPress={()=>{console.log("pressionamento longo")}}
+          >Open up App.tsx to start working on your app!
+          </Text>
+        <StatusBar style="auto" />
+        
+        <Text>
+          <Text>ola</Text>
+          <Text> mundo</Text>
+        </Text>
+
+        <View>
+          <Text>ola</Text>
+          <Text>mundo</Text>
+        </View>
+
+      </View>
+    </ScrollView>
   );
 }
 
@@ -17,4 +82,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input:{
+    height:40,
+    margin:12,
+    borderWidth:1,
+    padding:10,
+    backgroundColor:'white'
+  },
+  border:{
+    borderColor: 'red',
+    borderWidth: 3
+  },
+  texto:{
+    fontSize:20,
+    fontWeight:'bold',
+    backgroundColor: 'blue',
+    color:'white',
+    padding:20,
+  }
 });
